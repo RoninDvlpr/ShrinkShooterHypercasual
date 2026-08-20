1bdb0ea6db392ad2f90136e61eebc31f4ab47da5
UnityAppsTest <unityappstestemail@gmail.com>
2026-08-20 15:58:27 +0300

    Refactored the shockwave effect to decouple she shockwave spheres animation from the effect's gloabls scale.

--- Assets/Scripts/Shooting/Shockwave.cs
+++ Assets/Scripts/Shooting/Shockwave.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 
 public class Shockwave : MonoBehaviour
 {
+    [SerializeField] Transform shockwavesParent;
     [Tooltip("The renderers that will be affected by the shockwave's alpha fade-out. The used shader is supposed to be the UnversalShader asset that has the '_GlobalAlpha' property.")]
     [SerializeField] List<Renderer> shockwaveRenderers;
     static readonly int GlobalAlphaPropertyID = Shader.PropertyToID("_GlobalAlpha");
@@ -31,36 +32,62 @@ public class Shockwave : MonoBehaviour
         propBlock = new MaterialPropertyBlock();
     }
 
+    /// <summary>
+    /// Launches the shockwave animation with the default duration and expansion.
+    /// </summary>
+    /// <param name="onCompleteCallback">Gets called after the animation ends</param>
     public void Play(Action onCompleteCallback = null)
     {
-        Play(transform.localScale.x * defaultExpansionCoefficient, onCompleteCallback);
+        float defaultBlastSize = shockwavesParent.lossyScale.x * defaultExpansionCoefficient;
+        Play(defaultBlastSize, onCompleteCallback);
     }
 
-    public void Play(float blastSize, Action onCompleteCallback = null)
+    /// <summary>
+    /// Launches the shockwave animation with the default duration.
+    /// </summary>
+    /// <param name="targetBlastSize">The global size the shockwave should have at the end of the animation</param>
+    /// <param name="onCompleteCallback">Gets called after the animation ends</param>
+    public void Play(float targetBlastSize, Action onCompleteCallback = null)
     {
-        Play(blastSize, defaultDuration, onCompleteCallback);
+        Play(targetBlastSize, defaultDuration, onCompleteCallback);
     }
 
-    public void Play(float startingBlastSize, float finalBlastSize, float duration, Action onCompleteCallback = null)
+    /// <summary>
+    /// Launches the shockwave animation with a specified starting size.
+    /// Used to match the shockwave size to the projectile size when the projectile is destroyed.
+    /// </summary>
+    /// <param name="startingBlastSize">The starting size of the shockwave effect</param>
+    /// <param name="targetBlastSize">The global size the shockwave should have at the end of the animation</param>
+    /// <param name="duration">The explosion animation duration</param>
+    /// <param name="onCompleteCallback">Gets called after the animation ends</param>
+    public void Play(float startingBlastSize, float targetBlastSize, float duration, Action onCompleteCallback = null)
     {
         transform.localScale = Vector3.one * startingBlastSize;
-        Play(finalBlastSize, duration, onCompleteCallback);
+        Play(targetBlastSize, duration, onCompleteCallback);
     }
 
-    public void Play(float blastSize, float duration, Action onCompleteCallback = null)
+    /// <summary>
+    /// Launches the shockwave animation.
+    /// </summary>
+    /// <param name="targetBlastSize">The global size the shockwave should have at the end of the animation</param>
+    /// <param name="duration">The explosion animation duration</param>
+    /// <param name="onCompleteCallback">Gets called after the animation ends</param>
+    public void Play(float targetBlastSize, float duration, Action onCompleteCallback = null)
     {
+        float localizedTargetBlastSize = CalculateShockwaveLocalBlastSize(targetBlastSize);
+
         Sequence explosionSequence = DOTween.Sequence();
 
         // Phase A: Rapid explosive expansion
         explosionSequence.Append(
-            transform.DOScale(blastSize, duration).SetEase(Ease.OutQuad)
+            shockwavesParent.DOScale(localizedTargetBlastSize, duration).SetEase(Ease.OutQuad)
         );
 
         // Phase B: Eased die-out (Slight scale over-shoot + alpha fade)
-        float overshootSize = blastSize * overshootCoefficient;
+        float overshootSize = localizedTargetBlastSize * overshootCoefficient;
         float fadeDuration = duration * fadeOutDurationCoefficient;
         explosionSequence.Append(
-            transform.DOScale(overshootSize, fadeDuration).SetEase(Ease.OutCubic)
+            shockwavesParent.DOScale(overshootSize, fadeDuration).SetEase(Ease.OutCubic)
         );
 
         // Parallel Alpha Fadeout using MaterialPropertyBlock
@@ -86,5 +113,11 @@ public class Shockwave : MonoBehaviour
         renderer.GetPropertyBlock(propBlock);
         propBlock.SetFloat(GlobalAlphaPropertyID, alpha);
         renderer.SetPropertyBlock(propBlock);
+    }
+
+    float CalculateShockwaveLocalBlastSize(float targetGlobalSize)
+    {
+        float scaleCoefficient = shockwavesParent.localScale.x / shockwavesParent.lossyScale.x;
+        return targetGlobalSize * scaleCoefficient;
     }
 }
